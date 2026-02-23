1f7726098a14435997dfa650c9aa9658d02b85e8
Siddhant Barshile <sidhantbarshile79@gmail.com>
2026-02-23 17:29:32 +0530

    git commit -e "Review Service impl"

--- naukri/src/main/java/com/naukri/review/ReviewServiceImpl.java
+++ naukri/src/main/java/com/naukri/review/ReviewServiceImpl.java
@@ -1,9 +1,12 @@
 package com.naukri.review;
 
+import com.naukri.company.Company;
 import com.naukri.company.CompanyService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
 public class ReviewServiceImpl implements ReviewService{
@@ -18,26 +21,54 @@ public class ReviewServiceImpl implements ReviewService{
 
     @Override
     public List<Review> getAllReviews(Long companyId) {
-        return List.of();
+        return reviewRepository.findAll();
+    }
+
+    //get the company object to add review
+
+    @Override
+    public boolean addReview (Long companyId, Review review) {
+        Company company =companyService.getCompanyById(companyId);
+        if (company != null) {
+            review.setCompany (company);
+            reviewRepository.save(review);
+            return true;
+        } else {
+            return false;
+        }
+
+
+        @Override
+        public Review getReview(Long companyId, Long reviewId){
+            List<Review> reviews reviewRepository.findByCompanyId(companyId);
+            return reviews.stream()
+                    .filter(review -> review.getId().equals(reviewId))
+                    .findFirst()
+                    .orElse(null);
+        }
+
+    @Override
+    public boolean updateReview(Long companyId, Long reviewId, Review updateReview) {
+        if (companyService.getCompanyById(companyId)!= null){
+            updateReview.setCompany(companyService.getCompanyById(companyId));
+            updateReview.setId(reviewId);
+            reviewRepository.save(updateReview);
+            return true;
+        }else {
+            return false;
+        }
     }
 
     @Override
-    public boolean addReview(Long companyId, Review review) {
-        return false;
-    }
-
-    @Override
-    public Review getReview(Long companyId, Long reviewId) {
-        return null;
-    }
-
-    @Override
-    public boolean updateReview(Long companyId, Long reviewId, Review review) {
-        return false;
-    }
-
-    @Override
-    public boolean deleteReview(Long companyId, Long reviewId) {
-        return false;
-    }
+    public boolean deleteReview (Long companyId, Long reviewId) {
+        if (companyService.getCompanyById(companyId)!= null && reviewRepository.existsById(reviewId)){
+            Review review= reviewRepository.findById(reviewId).orElse (null);
+            Company company= review.getCompany();
+            company.getReviews().remove(review);
+            companyService.updateCompany(company, companyId);
+            reviewRepository.deleteById(reviewId);
+            return true;
+        }else {
+            return false;
+        }
 }
